4cf0268ff5d417df0930f3de9e6e48bdaad80210
dolphinderby <129580551+dolphinderby@users.noreply.github.com>
2023-05-06 14:59:17 +0000

    [now]13章あたり

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,24 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:book_name, :caption)
   end
 end
