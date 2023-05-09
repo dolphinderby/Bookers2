bccb2e30119992b2e853b2b952e2b73ae51e1e44
dolphinderby <129580551+dolphinderby@users.noreply.github.com>
2023-05-09 15:34:24 +0000

    修正中

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,13 @@
 class Book < ApplicationRecord
   has_one_attached :profile_image
   belongs_to :user
+
+  def book
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
+  
 end
