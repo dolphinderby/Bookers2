7d87483cb7096569df0867036d1de1b0baeb777f
dolphinderby <129580551+dolphinderby@users.noreply.github.com>
2023-05-13 09:28:21 +0000

    レイアウト中

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "homes#top"
+  get "home/about" => "homes#about"
 
   resources :books
   resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/homes/about" => "homes#about", as: "about"
+
 
 end
