6dc2af59197649677cb530ebd83808bbf03ba8fe
dolphinderby <129580551+dolphinderby@users.noreply.github.com>
2023-05-14 13:10:02 +0000

    作業中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,6 +25,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @new = Book.new
   end
 
   def edit
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,16 +1,9 @@
 class Book < ApplicationRecord
-  has_one_attached :profile_image
+
   belongs_to :user
 
   validates :title, presence: true
   validates :body, presence: true, length: {maximum: 200 }
 
-  def book
-    if image.attached?
-      image
-    else
-      'no_image.jpg'
-    end
-  end
 
 end
